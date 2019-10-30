26b9898f86f54dc55c25114c73bd79609d936a25
gs456456 <1154442437@qq.com>
2019-10-30 22:20:15 +0800

    day1

--- Day1/pubSub.js
+++ Day1/pubSub.js
@@ -49,7 +49,29 @@ a.cancel('company',isPlaying)
 
 a.publish('company','mike','厕所')
 
-// a.publish('')
+
+class shoppingCart extends LkEvent{
+	constructor(){
+		super()
+		this.shoppingCartList = []
+	}
+	// addProduct(product){
+	// 	this.shoppingCartList.push(product)
+	// }
+	caculateTotalDiscount(product){
+		console.log('计算' + product + '折扣')
+	}
+	caculateTotalPrice(product){
+		console.log('计算' + product + '总价')
+	}
+}
+
+var shoppingCartIns = new shoppingCart()
+shoppingCartIns.subscribe('addProduct',shoppingCartIns.caculateTotalDiscount)
+shoppingCartIns.subscribe('addProduct',shoppingCartIns.caculateTotalPrice)
+shoppingCartIns.publish('addProduct','苹果')
+shoppingCartIns.publish('addProduct','香蕉')
+
 
 
 
